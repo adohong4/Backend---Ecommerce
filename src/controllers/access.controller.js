4fb4f72e4ee0c128835c244f437a0fcb5ab9e972
adohong4 <adohong4@gmail.com>
2024-08-14 00:47:53 +0700

    Success.response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,19 +2,18 @@
 
 const AccessService = require("../services/access.service")
 
+const { OK, CREATED } = require('../core/success.response')
+
 class AccessController {
     signUp = async (req, res, next) => {
-        // try {
-        //     console.log(`[P]::signUp::`, req.body)
-        //     /*
-        //         200 OK
-        //         201 CREATED
-        //     */
-        //     return res.status(201).json(await AccessService.signUp(req.body))
-        // } catch (error) {
-        //     next(error)
-        // }
-        return res.status(201).json(await AccessService.signUp(req.body))
+        // return res.status(201).json({
+        //     message: '',
+        //     metadata:
+        // })
+        new CREATED({
+            message: 'Registered OK',
+            metadata: await AccessService.signUp(req.body)
+        }).send(res)
     }
 }
 
